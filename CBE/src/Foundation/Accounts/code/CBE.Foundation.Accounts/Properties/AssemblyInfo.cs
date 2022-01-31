2fd85692d364b0e1122803c37dbd0abf7ff63057
Ali Adel <Ali.Adel@linkdev.com>
2022-01-31 23:38:52 +0200

    Commit

--- CBE/src/Foundation/Accounts/code/CBE.Foundation.Accounts/Properties/AssemblyInfo.cs
+++ CBE/src/Foundation/Accounts/code/CBE.Foundation.Accounts/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5181fac8-e723-459d-b5f4-e9ab203b2e0a")]
+[assembly: Guid("35d2efab-822a-4c49-a6e2-dd71cf425a50")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ CBE/src/Foundation/Accounts/code/old/CBE.Foundation.Accounts/Providers/IContactFacetsProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace CBE.Foundation.Accounts.Providers
+{
+    using Sitecore.Analytics.Tracking;
+    using Sitecore.XConnect.Collection.Model;
+
+    public interface IContactFacetsProvider
+    {
+        Contact Contact { get; }
+        Sitecore.XConnect.Collection.Model.KeyBehaviorCache KeyBehaviorCache { get; }
+        PersonalInformation PersonalInfo { get; }
+        AddressList Addresses { get; }
+        EmailAddressList Emails { get; }
+        ConsentInformation CommunicationProfile { get; }
+        PhoneNumberList PhoneNumbers { get; }
+        Avatar Picture { get; }
+        bool IsKnown { get; }
+    }
+}
